a1e67403b634824d549f47016370e81a7cdc0fe5
malchak <michaelmalchak@gmail.com>
2013-11-23 13:08:32 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hansel::Application.config.secret_key_base = 'ef4bb5540caa47ada6285bb9ab36461589fb480283c32a2037dae1fc2e1f6b384532d4c51e5510a5b848e770bff1382ebdd8a7231826f66a60abd921fd9d0468'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hansel::Application.config.session_store :cookie_store, key: '_hansel_session'
