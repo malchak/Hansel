8a63c272aa33dc8c6a32d4a2f0e52497134a574b
malchak <michaelmalchak@gmail.com>
2013-11-23 19:44:16 -0800

    bootstrap ember dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require hansel
+
+// for more details see: http://emberjs.com/guides/application/
+Hansel = Ember.Application.create();
+
 //= require_tree .
